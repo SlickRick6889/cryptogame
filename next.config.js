4edbbbd17f1dc197b4eff2f84050465cff1db81c
SlickRick6889 <jrmachantenterprise@gmail.com>
2025-06-16 22:00:53 -0400

    feat(firebase): embed client config in next.config.js and simplify firebase.ts

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_DEMO_MODE: 'false',
+    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyATBlYvA_dZrQNSdC8F3v7mu8mFYZQAGes",
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "website-6889.firebaseapp.com",
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "website-6889",
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "website-6889.firebasestorage.app",
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "606064079207",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "1:606064079207:web:35dd32a23d0443a873ffc2",
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-5K76PRT0NZ",
   },
   eslint: {
     ignoreDuringBuilds: true,
